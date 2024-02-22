ba9b682a0be7df2a366abdf15e4fad1e3e56d5b5
project1 <duyndph43896@fpt.edu.vn>
2024-02-22 22:21:08 +0700

    duy

---
+++ server/app/Models/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_user',
+        'totalQuantity',
+        'paymentMethod',
+        'time',
+        'paymentStatus'
+    ] ;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'id_user');
+    }
+}
