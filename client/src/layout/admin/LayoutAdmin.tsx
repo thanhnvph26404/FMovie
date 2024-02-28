8191c44a570567d255c3122a10b84cb817886219
thanh.nv <developer@innofly-software.com>
2024-02-28 21:14:25 +0700

    layout admin

--- client/src/layout/admin/LayoutAdmin.tsx
+++ client/src/layout/admin/LayoutAdmin.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './LayoutAdmin.scss'
-import { Outlet } from 'react-router-dom'
-import { HeaderAdmin } from '../../components'
+import { HeaderAdmin } from "../../components";
+import SideBar from "../../components/side-bar/SideBar";
+import "./LayoutAdmin.scss";
+import { Outlet } from "react-router-dom";
 
 const LayoutAdmin = () => {
-  return (
-      <div>
-          <HeaderAdmin/>
-          <Outlet />
-      </div>
-  )
-}
+    return (
+        <>
+            <div className="w-full flex h-full">
+                <div className="w-1/5">
+                    <SideBar />
+                </div>
+                <div className="w-full h-[8000px]">
+                    <HeaderAdmin />
+                    <div className="px-[4rem] pt-[4rem]">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default LayoutAdmin
+export default LayoutAdmin;
--- client/src/pages/admin/dashboard/Dashboard.tsx
+++ client/src/pages/admin/dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Dashboard.scss'
+
 const Dashboard = () => {
   return (
     <div>Dashboard</div>
