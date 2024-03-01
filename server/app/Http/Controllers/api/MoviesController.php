268d6761841bfabe45873ee9b736509679bd7121
nguyenhuutan44525 <riven1023531021899@gmail.com>
2024-03-01 14:16:51 +0700

    tìm kiếm theo tên, lọc theo danh mục

--- server/app/Http/Controllers/api/MoviesController.php
+++ server/app/Http/Controllers/api/MoviesController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\api;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Category;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\MoviesResource;
 use App\Models\Movies;
 use Illuminate\Support\Str;
+
 class MoviesController extends Controller
 {
     /**
@@ -30,19 +34,17 @@ class MoviesController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-
-    ]);
-    if ($validator->fails()) {
-        return response()->json([
-            'status' => 422,
-            'message' => $validator->messages()
-        ], 422);
-    }
+    {
+        $validator = Validator::make($request->all(), []);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        }
         $movies = Movies::create($request->all());
         return new MoviesResource($movies);
-}
+    }
 
 
     /**
@@ -51,10 +53,9 @@ class MoviesController extends Controller
     public function show(string $id)
     {
         $movies = Movies::find($id);
-        if(!$movies){
-            return response()->json(['message'=>'Không tìm thấy ']);
-        }
-        else{
+        if (!$movies) {
+            return response()->json(['message' => 'Không tìm thấy ']);
+        } else {
             return new MoviesResource($movies);
         }
     }
@@ -73,25 +74,58 @@ class MoviesController extends Controller
     public function update(Request $request, string $id)
     {
         $movies = Movies::find($id);
-        if(!$movies){
-            return response()->json(['message'=>'Không tìm thấy movies']);
+        if (!$movies) {
+            return response()->json(['message' => 'Không tìm thấy movies']);
         }
         $movies->update($request->all());
         return new MoviesResource($movies);
     }
 
-    //     public function search(Request $request)
-    // {
-    //     $name = $request->input('name'); // Lấy giá trị của tham số name từ request
-    //     $movies = Movies::where('name', 'like', "%$name%")->get();
+    public function search(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);
 
-    //     if ($movies->isEmpty()) {
-    //         return response()->json(['message' => 'Không tìm thấy phim nào có tiêu đề tương tự'], 404);
-    //     }
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        }
 
-    //     return MoviesResource::collection($movies);
-    // }
+        $name = $request->input('name');
+
+        $movies = Movies::where('name', 'like', "%$name%")->get();
+
+        if ($movies->isEmpty()) {
+            return response()->json(['message' => 'Không tìm thấy phim nào có tiêu đề tương tự'], 404);
+        }
+
+        return MoviesResource::collection($movies);
+    }
 
 
+    /**
+     * Lọc phim theo danh mục.
+     *
+     * @param  string  $categoryName
+     * @return \Illuminate\Http\Response
+     */
+    public function filterByCategory($categoryName)
+    {
+        // Tìm danh mục theo tên
+        $category = Category::where('name', $categoryName)->first();
 
+        // Nếu không tìm thấy danh mục, trả về một thông báo lỗi
+        if (!$category) {
+            return response()->json(['message' => 'Không tìm thấy danh mục'], 404);
+        }
+
+        // Lọc phim theo danh mục
+        $movies = Movies::where('id_category', $category->id)->get();
+
+        // Trả về danh sách phim dưới dạng JSON
+        return MoviesResource::collection($movies);
+    }
 }
