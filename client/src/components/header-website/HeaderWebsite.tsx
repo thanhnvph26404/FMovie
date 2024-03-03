9941db6d0712849fa1502e7996246cddb9afc1b1
Nguyen Duc <tundph26921@fpt.edu.vn>
2024-03-03 20:09:27 +0700

    u

--- client/src/components/header-website/HeaderWebsite.tsx
+++ client/src/components/header-website/HeaderWebsite.tsx
@@ -44,8 +44,8 @@ const HeaderWebsite = () => {
                           <a href="#"></a>
                       </div>
                       <ul className="list-unstyled list-inline pull-right">
-                          <li><a href="#">Đăng nhập</a></li>
-                          <li><a href="#">Đăng ký</a></li>
+                          <li><a href="Login">Đăng nhập</a></li>
+                          <li><a href="register">Đăng ký</a></li>
                       </ul>
                   </div>
               </div>
