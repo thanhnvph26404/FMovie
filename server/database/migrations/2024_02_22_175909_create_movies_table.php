1b3f20eb32ce3b648a0ef906b0eb057579c2cff6
project1 <duyndph43896@fpt.edu.vn>
2024-03-11 19:20:00 +0700

    update

--- server/database/migrations/2024_02_22_175909_create_movies_table.php
+++ server/database/migrations/2024_02_22_175909_create_movies_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->enum('status', ['Phim sắp chiếu', 'Phim đang chiếu', 'Suất chiếu đặc biệt']);
             $table->string('time');
             $table->string('director');
             $table->string('actor');
